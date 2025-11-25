cdb4e729d18bd6d8421eff899531ba52bc8c7a5c
aslaf <aslaf.dev@gmail.com>
2025-11-25 15:46:14 +0100

    main.py

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,4 @@ from src.app import app
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("src.app:app", host="127.0.0.1", port=8000, reload=True)
